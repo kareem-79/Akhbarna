cf7879e6fae7ed0dc486ddac314b5c0482b246e0
Kareem Mohamed <Kareemmohamed2467@gmail.com>
2026-02-16 19:36:52 +0200

    Build App

---
+++ android/app/src/main/kotlin/com/akhbarna/akhbarna/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.akhbarna.akhbarna
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'features/layout/main_layout.dart';
+
+void main() {
+  runApp(const Akhbarna());
+}
+
+class Akhbarna extends StatelessWidget {
+  const Akhbarna({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      designSize: Size(393, 852),
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Akhbarna',
+        home: MainLayout(),
+      ),
+    );
+  }
+}
